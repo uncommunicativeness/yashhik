257c8a3066b1afae12ae1d5a32e29d2f01f67e05
Alex Filatov <alex.filatov@live.com>
2021-02-26 09:27:36 +0300

    Added headers

--- yandex.py
+++ yandex.py
@@ -1,19 +1,36 @@
 import json
 
 import requests
+from fake_useragent import UserAgent
 
 import config
 
 
 def send_to_screen(video_url):
+    headers = {
+        'method': 'GET',
+        'scheme': 'https',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+        'cache-control': 'max-age=0',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': UserAgent().random,
+    }
+
     auth_data = {
         'login': config.login,
         'passwd': config.password
     }
 
     s = requests.Session()
-    s.get("https://passport.yandex.ru/")
-    s.post("https://passport.yandex.ru/passport?mode=auth&retpath=https://yandex.ru", data=auth_data)
+    s.get("https://passport.yandex.ru/", headers=headers)
+    s.post(
+        "https://passport.yandex.ru/passport?mode=auth&retpath=https://yandex.ru",
+        data=auth_data,
+        headers=headers
+    )
 
     Session_id = s.cookies["Session_id"]
 
